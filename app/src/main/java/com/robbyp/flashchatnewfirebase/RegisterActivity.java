e878afa4c493b3b4b5eaae78cfbdb6e01baefd4e
Rob Peschke <rspesc31@gmail.com>
2021-01-12 09:55:27 -0500

    delete other method that also works with firebase

--- app/src/main/java/com/robbyp/flashchatnewfirebase/RegisterActivity.java
+++ app/src/main/java/com/robbyp/flashchatnewfirebase/RegisterActivity.java
@@ -126,30 +126,6 @@ public class RegisterActivity extends AppCompatActivity {
         Log.d("FlashChat", "This is your email:     " + email);
         Log.d("FlashChat", "This is your password:  " + password);
 
-//        mAuth.createUserWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Log.d("FlashChat", "createUserWithEmail:success");
-//                            FirebaseUser user = mAuth.getCurrentUser();
-//                            //updateUI(user);
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Log.w("FlashChat", "createUserWithEmail:failure", task.getException());
-////                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-////                                    Toast.LENGTH_SHORT).show();
-////                                    showErrorDialog("Registration Attempt Failed");
-//
-//                            //updateUI(null);
-//                        }
-//
-//                        // [START_EXCLUDE]
-//                        //hideProgressBar();
-//                        // [END_EXCLUDE]
-//                    }
-//                });
 
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
