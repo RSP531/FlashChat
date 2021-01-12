9849e0b978feb76acd3d50c55de18c2749547f55
Rob Peschke <rspesc31@gmail.com>
2021-01-12 09:49:38 -0500

    both methods of createUserWithEmailAndPassword work correctly and will create a new user with firebase. I suspect this code does not work on company computer and cannot get through the firewall

--- app/src/main/java/com/robbyp/flashchatnewfirebase/RegisterActivity.java
+++ app/src/main/java/com/robbyp/flashchatnewfirebase/RegisterActivity.java
@@ -126,44 +126,44 @@ public class RegisterActivity extends AppCompatActivity {
         Log.d("FlashChat", "This is your email:     " + email);
         Log.d("FlashChat", "This is your password:  " + password);
 
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("FlashChat", "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("FlashChat", "createUserWithEmail:failure", task.getException());
-//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                                    Toast.LENGTH_SHORT).show();
-//                                    showErrorDialog("Registration Attempt Failed");
-
-                            //updateUI(null);
-                        }
-
-                        // [START_EXCLUDE]
-                        //hideProgressBar();
-                        // [END_EXCLUDE]
-                    }
-                });
-
-//        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//            @Override
-//            public void onComplete(@NonNull Task<AuthResult> task) {
-//                Log.d("FlashChat", "Create user onComplete: " + task.isSuccessful());
+//        mAuth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d("FlashChat", "createUserWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            //updateUI(user);
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                            Log.w("FlashChat", "createUserWithEmail:failure", task.getException());
+////                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+////                                    Toast.LENGTH_SHORT).show();
+////                                    showErrorDialog("Registration Attempt Failed");
 //
-//                if(!task.isSuccessful()){
-//                    Log.d("FlashChat", "user registration failed");
-//                    showErrorDialog("Registration Attempt Failed");
-//                } else {
-//                    Log.d("FlashChat", "user should be registered");
-//                }
-//            }
-//        });
+//                            //updateUI(null);
+//                        }
+//
+//                        // [START_EXCLUDE]
+//                        //hideProgressBar();
+//                        // [END_EXCLUDE]
+//                    }
+//                });
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("FlashChat", "Create user onComplete: " + task.isSuccessful());
+
+                if(!task.isSuccessful()){
+                    Log.d("FlashChat", "user registration failed");
+                    showErrorDialog("Registration Attempt Failed");
+                } else {
+                    Log.d("FlashChat", "user should be registered");
+                }
+            }
+        });
     }
 
     // TODO: Save the display name to Shared Preferences
